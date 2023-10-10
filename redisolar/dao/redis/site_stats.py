a5f7ffdfa8f6ac7e00a5aaa8f2c0ddcb7be57f92
Azat <azat4dev@gmail.com>
2023-10-10 13:52:59 +0400

    Complete challenge #3

--- redisolar/dao/redis/site_stats.py
+++ redisolar/dao/redis/site_stats.py
@@ -60,8 +60,32 @@ class SiteStatsDaoRedis(SiteStatsDaoBase, RedisDaoBase):
             pipeline = self.redis.pipeline()
             execute = True
 
-        # START Challenge #3
-        # END Challenge #3
+        reporting_time = datetime.datetime.utcnow().isoformat()
+
+        self.redis.hset(key, SiteStats.LAST_REPORTING_TIME, reporting_time)
+        self.redis.hincrby(key, SiteStats.COUNT, 1)
+        self.redis.expire(key, WEEK_SECONDS)
+
+        self.compare_and_update_script.update_if_greater(
+            pipeline,
+            key,
+            SiteStats.MAX_WH,
+            meter_reading.wh_generated
+        )
+
+        self.compare_and_update_script.update_if_less(
+            pipeline, 
+            key, 
+            SiteStats.MIN_WH, 
+            meter_reading.wh_generated
+        )
+
+        self.compare_and_update_script.update_if_greater(
+            pipeline, 
+            key, 
+            SiteStats.MAX_CAPACITY, 
+            meter_reading.current_capacity
+        )
 
         if execute:
             pipeline.execute()
@@ -69,9 +93,6 @@ class SiteStatsDaoRedis(SiteStatsDaoBase, RedisDaoBase):
     def update(self, meter_reading: MeterReading, **kwargs) -> None:
         key = self.key_schema.site_stats_key(meter_reading.site_id,
                                              meter_reading.timestamp)
-        # Remove for Challenge #3
-        self._update_basic(key, meter_reading)
-
-        # Uncomment the following two lines for Challenge #3
-        # pipeline = kwargs.get('pipeline')
-        # self._update_optimized(key, meter_reading, pipeline)
+        
+        pipeline = kwargs.get('pipeline')
+        self._update_optimized(key, meter_reading, pipeline)
